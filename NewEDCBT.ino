563f3393477bfb33fdc3c3adcb940324172d63aa
mitsalgh <121527453+mitsalgh@users.noreply.github.com>
2024-07-19 09:13:35 +0700

    Update NewEDCBT.ino

--- NewEDCBT.ino
+++ NewEDCBT.ino
@@ -17,41 +17,83 @@ const unsigned long checkInterval = 1000;   // Interval untuk memeriksa koneksi 
 const unsigned long nfcReadTimeout = 1000;  // Timeout untuk pembacaan NFC
 
 #define ledBT 4
-#define ledBat 31
+#define ledBat 25
+// #define gndLedBat 2
 #define buzzer 27
+#define BATTERY_PIN 32
 
-// Baterai Setup 
-#define bateraiIndikator 32
-#define BATTERY_PIN 32  // Pin untuk deteksi baterai
-#define ADC_MAX_VALUE 4095  // Nilai maksimum ADC untuk ESP32
-#define VOLTAGE_DIVIDER_RATIO 2.0  // Rasio pembagi tegangan (1kΩ / (1kΩ + 1kΩ))
-#define REFERENCE_VOLTAGE 3.3  // Tegangan referensi ESP32 (umumnya 3.3V)
-
-#define FULL_BATTERY_VOLTAGE 4.2  // Tegangan baterai penuh
-#define EMPTY_BATTERY_VOLTAGE 3.0  // Tegangan baterai kosong 
+const int ledValue = 25;
+// Baterai Setup
+// #define bateraiIndikator 32
+// const int BATTERY_PIN = 32; // Pin ADC yang digunakan untuk membaca tegangan
+const float maxVoltage = 4.1;        // Tegangan maksimal
+const float minVoltage = 3.4;        // Tegangan minimal
+const float referenceVoltage = 4.1;  // Tegangan referensi
+const int adcMaxValue = 4095;        // Nilai maksimum ADC (12-bit ADC)
+const float ralatBaterai = 0.04;
+const int numSamples = 500;  // Jumlah sampel untuk averaging
 
 String nfcID = "";
 
-void bateraiIndikator()
-{
-  int rawValue = analogRead(BATTERY_PIN);
-  float batteryVoltage = (rawValue * REFERENCE_VOLTAGE) / ADC_MAX_VALUE * VOLTAGE_DIVIDER_RATIO; // rumus baterai rasio ke
-  
-  // Konversi tegangan baterai ke persentase
-  float batteryPercentage = ((batteryVoltage - EMPTY_BATTERY_VOLTAGE) / (FULL_BATTERY_VOLTAGE - EMPTY_BATTERY_VOLTAGE)) * 100;
-  
-  // Batasi nilai persentase antara 0 dan 100
-  batteryPercentage = constrain(batteryPercentage, 0, 100);
+void bateraiIndikator() {
+  long adcSum = 0;
+  for (int i = 0; i < numSamples; i++) {
+    adcSum += analogRead(BATTERY_PIN);
+    delay(1);  // Delay pendek untuk memberikan waktu antara pembacaan sampel
+  }
 
-  Serial.print("Battery Voltage: ");
-  Serial.print(batteryVoltage);
+  int adcValue = adcSum / numSamples;
+  float voltage = (adcValue * referenceVoltage) / adcMaxValue;
+
+  if (voltage > maxVoltage) voltage = voltage;
+  if (voltage < minVoltage) voltage = voltage;
+
+  int percentage = ((voltage - minVoltage) / (maxVoltage - minVoltage)) * 100;
+  if(percentage <= 20)
+  {
+    analogWrite(ledBat,ledValue);
+    // digitalWrite(gndLedBat,LOW);
+  }
+  else if(percentage >= 80)
+  {
+    analogWrite(ledBat, 0);
+    Serial.println("masuk");
+    // digitalWrite(gndLedBat,LOW);
+  }
+  // for(int x=0;x < numSamples; x++)
+  // {
+
+  // }
+  // if (percentage > 20)
+  // {
+  //   digitalWrite(ledBat,LOW);
+  // }
+  // else if(percentage <=20)
+  // {
+  //   for(int x = 0;x<=200;x++)
+  //   {
+  //     analogWrite(ledBat,x);
+  //     delay(30);
+  //   }w
+  //   for(int x = 200;x>=0;x--)
+  //   {
+  //     analogWrite(ledBat,x);
+  //     delay(30);
+  //   }
+  // }
+
+  // Print ke Serial Monitor
+  Serial.print("ADC : ");
+  Serial.print(adcValue);
+  Serial.print(" ");
+  Serial.print("Voltage: ");
+  Serial.print(voltage);
   Serial.print(" V, ");
   Serial.print("Battery Percentage: ");
-  Serial.print(batteryPercentage);
+  Serial.print(percentage);
+  // SerialBT.println(voltage);
+  // SerialBT.println(percentage);
   Serial.println(" %");
-  SerialBT.print(batteryPercentage);
-  digitalWrite(ledBat,HIGH);
-  delay(1000);  // Tunggu 1 detik sebelum membaca kembali
 }
 
 void setup(void) {
@@ -60,8 +102,9 @@ void setup(void) {
   Serial.println("NFC reader initializing...");
   pinMode(ledBT, OUTPUT);
   pinMode(buzzer, OUTPUT);
-  pinMode(bateraiIndikator, INPUT);
-  pinMode(ledBat,OUTPUT);
+  pinMode(BATTERY_PIN, INPUT);
+  pinMode(ledBat, OUTPUT);
+  // pinMode(gndLedBat,OUTPUT);
   analogReadResolution(12);  // ADC diubah jadi 12-bit
   nfc.begin();
 
@@ -73,30 +116,43 @@ void setup(void) {
   }
   nfc.SAMConfig();
   Serial.println("System Ready Waiting for an NFC card ...");
+  // digitalWrite(gndLedBat,LOW);
   for (int x = 1; x <= 3; x++) {
-    digitalWrite(ledBT, HIGH);
-    digitalWrite(buzzer, HIGH);
-    delay(100);
+    analogWrite(ledBT, ledValue);
+    analogWrite(ledBat, ledValue);
     digitalWrite(buzzer, LOW);
-    digitalWrite(ledBT, LOW);
+    delay(100);
+    digitalWrite(buzzer, HIGH);
+    analogWrite(ledBT, 0);
+    analogWrite(ledBat, 0);
     delay(100);
   }
 }
 
 void loop(void) {
+  // while(1)
+  // {
+    
+  //   analogWrite(ledBat,ledValue);
+  //   // digitalWrite(gndLedBat,LOW);
+  //   delay(300);
+  //   analogWrite(ledBat,0);
+  //   // digitalWrite(gndLedBat,LOW);
+  //   delay(300);
+  // }
   unsigned long currentMillis = millis();
 
   // Memeriksa koneksi Bluetooth setiap interval tertentu
   if (currentMillis - lastCheck >= checkInterval) {
     lastCheck = currentMillis;
-    bluetoothConnected = SerialBT.hasClient();
     bateraiIndikator();
+    bluetoothConnected = SerialBT.hasClient();
   }
 
   if (bluetoothConnected) {
     uint8_t uid[7];  // Maximum size of the UID
     uint8_t uidLength;
-    bateraiIndikator();
+    // bateraiIndikator();
 
     // Memulai pendeteksian non-blocking
     nfc.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A);
@@ -141,42 +197,46 @@ void loop(void) {
         nfcID = CardID;
       }
       nfcID = CardID;
-      digitalWrite(buzzer, HIGH);
-      delay(300);
-      digitalWrite(buzzer, LOW);
-      delay(300);
+      delay(50);
+
+        digitalWrite(buzzer, LOW);
+        delay(300);
+        digitalWrite(buzzer, HIGH);
+        delay(300);
+      
       digitalWrite(ledBT, LOW);
       delay(100);
       Serial.println(nfcID);
       SerialBT.print(nfcID + String("\n"));
-      digitalWrite(ledBT,HIGH);
+      analogWrite(ledBT, ledValue);
     }
   } else {
     Serial.println("Bluetooth disconnected, waiting for connection...");
     for (int x = 1; x <= 4; x++) {
-      digitalWrite(buzzer, HIGH);
-      delay(200);
       digitalWrite(buzzer, LOW);
       delay(200);
+      digitalWrite(buzzer, HIGH);
+      delay(200);
     }
-    
+
     while (!SerialBT.hasClient()) {
       delay(500);  // Menunggu hingga ada koneksi Bluetooth
-      digitalWrite(ledBT,HIGH);
+      analogWrite(ledBT, ledValue);
       delay(100);
-      digitalWrite(ledBT,LOW);
+      analogWrite(ledBT, 0);
       delay(100);
+      bateraiIndikator();
+      delay(1000);
     }
     Serial.println("Bluetooth connected, resuming NFC reading...");
-    
-    digitalWrite(ledBT, HIGH);
+
+    analogWrite(ledBT, ledValue);
     for (int x = 1; x <= 3; x++) {
-      digitalWrite(buzzer, HIGH);
-      delay(100);
       digitalWrite(buzzer, LOW);
-      delay(100);
+      delay(200);
+      digitalWrite(buzzer, HIGH);
+      delay(200);
     }
-    
   }
   delay(500);  // Delay untuk menghindari loop terlalu cepat
 }
