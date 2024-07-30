bc07a424bfd60e85db4feb5988d135f93f0e9414
rann55 <rafieka23451@gmail.com>
2024-07-30 09:32:33 +0700

    lane 83

--- NewEDCBT.ino
+++ NewEDCBT.ino
@@ -80,7 +80,7 @@ void bateraiIndikator() {
 
 void setup(void) {
   Serial.begin(115200);
-  SerialBT.begin("rafi gwenchana");  // Nama Bluetooth
+  SerialBT.begin("ajikrally");  // Nama Bluetooth
   Serial.println("NFC reader initializing...");
   pinMode(ledBT, OUTPUT);
   pinMode(buzzer, OUTPUT);
